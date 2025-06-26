3d3a00c713a9d42d19ce0cb21c12b5875ec61a62
Ch Yogeesh Sai <108213999+Chyogeesh@users.noreply.github.com>
2025-06-27 02:06:13 +0530

    Update .js

--- .js
+++ .js
@@ -1,1 +1,9 @@
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const authRoutes = require("./routes/auth");
+app.use("/api/auth", authRoutes);
+{
+  "name": "Test User",
+  "email": "test@example.com",
+  "password": "test123",
+  "role": "Commander"
+}
