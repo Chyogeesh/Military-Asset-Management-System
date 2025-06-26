103911e7c7fc841645ffd5273d6120a514ff04cb
Ch Yogeesh Sai <108213999+Chyogeesh@users.noreply.github.com>
2025-06-27 02:02:07 +0530

    Update json.js

--- json.js
+++ json.js
@@ -4,3 +4,15 @@
   "password": "admin123",
   "role": "Admin"
 }
+{
+  "name": "Commander Alpha",
+  "email": "commander@example.com",
+  "password": "cmd123",
+  "role": "Commander"
+}
+{
+  "name": "Logistics Officer",
+  "email": "logistics@example.com",
+  "password": "log123",
+  "role": "Logistics"
+}
