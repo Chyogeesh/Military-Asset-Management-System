62ad937e89fffbe422ee2beedd27d7f0574c94a9
Ch Yogeesh Sai <108213999+Chyogeesh@users.noreply.github.com>
2025-06-27 02:04:26 +0530

    Update json.js

--- json.js
+++ json.js
@@ -16,3 +16,33 @@
   "password": "log123",
   "role": "Logistics"
 }
+{
+  "name": "Base Alpha",
+  "location": "North Sector"
+}
+{
+  "name": "AK-47",
+  "assetType": "Weapon",
+  "quantity": 50,
+  "base": "<BASE_ID>"
+}
+{
+  "asset": "<ASSET_ID>",
+  "quantity": 10,
+  "base": "<BASE_ID>"
+}
+{
+  "asset": "<ASSET_ID>",
+  "quantity": 5,
+  "fromBase": "<BASE_1>",
+  "toBase": "<BASE_2>"
+}
+{
+  "asset": "<ASSET_ID>",
+  "quantity": 2,
+  "assignedTo": "Soldier X"
+}
+{
+  "assetId": "<ASSET_ID>",
+  "quantity": 1
+}
